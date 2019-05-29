dec2438e9179fab9e729b846f29fcf68418164e9
trans <trans@localhost.localdomain>
2019-05-29 08:59:07 +0800

    update django

---
+++ Expression/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ExpressionConfig(AppConfig):
+    name = 'Expression'
---
+++ Expression/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def get(request):
+    a = request.POST['a']
+    b = request.POST['b']
+    print(a)
+    print(b)
+    return render(request,'index.html')
+
+def uploadFile(request):
+    if request.method == "POST":  # 请求方法为POST时，进行处理
+        myFile = request.FILES.get("img", None)  # 获取上传的文件，如果没有文件，则默认为None
+        print(myFile.name)
+        if not myFile:
+            return HttpResponse("no files for upload!")
+        import os
+        destination = open(os.path.join("D:\\upload", myFile.name), 'wb+')  # 打开特定的文件进行二进制的写操作
+        for chunk in myFile.chunks():  # 分块写入文件
+            destination.write(chunk)
+        destination.close()
+
+        #表情识别接口
+
+
+        #返回表情识别结果
+        return HttpResponse("成功")
